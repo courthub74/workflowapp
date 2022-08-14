f7db33bc86c220b23bdebf6dfae3be9be3cc896d
Courdevelops <courdevelops@gmail.com>
2022-08-14 17:15:08 -0400

    submit button added

--- src/App.js
+++ src/App.js
@@ -18,9 +18,16 @@ function App() {
           <h1>This is the Workflow Machine</h1>
 
           <div className='inputs'>
+            <p className='login'>Login</p>
             <input className='user' placeholder='username'></input>
             <input className='password' placeholder='password'></input>
           </div>
+
+          <div className='submitbutton'>
+            <button className='submit'>Submit</button>
+          </div>
+
+          
         </div>
       </div>
       
