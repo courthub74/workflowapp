c734b6a54bc7232ba8c6e17eb532cbb133762b27
Courdevelops <courdevelops@gmail.com>
2022-08-17 19:12:05 -0400

    Login almost set

--- src/App.js
+++ src/App.js
@@ -20,16 +20,21 @@ function App() {
 
 
             <img className='Courdevelops' src={Courdevelops} alt="Logo"/>
-            <h1>This is the Workflow Machine</h1>
+            <p className='headliner'>This is the Workflow Machine</p>
 
-            <div className='inputs'>
-              <p className='login'>Login</p>
-              <input className='user' placeholder='username'></input>
-              <input className='password' placeholder='password'></input>
-            </div>
 
-            <div className='submitbutton'>
-              <button className='submit'>Submit</button>
+            <div className='loginfield'>
+
+                <div className='inputs'>
+                  <p className='login'>Login</p>
+                  <input className='user' placeholder='username'></input>
+                  <input className='password' placeholder='password'></input>
+                </div>
+
+                <div className='submitbutton'>
+                  <button className='submit'>Submit</button>
+                </div>
+
             </div>
 
 
